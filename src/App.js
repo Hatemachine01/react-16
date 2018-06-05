93c3149973ca6b3a65800be0056947f3ae0881d2
Julio <juliodevasquez@gmail.com>
2018-06-04 20:08:59 -0400

    Added Person component + eventhandlers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Yadira' , age: 28},
+      {name: 'Julio' , age: 25},
+      {name: 'Renata' , age: 0},   
+    ]
+  }
+
+  switchNameHandler = () => {
+    // this.state.persons[0].name = 'Julius';
+    this.setState({
+      persons: [
+      {name: 'Julius' , age: 28},
+      {name: 'Yadira' , age: 25},
+      {name: 'Renata' , age: 0}, 
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1> It's me, mario! </h1>
+        <p> This is really working </p>
+        <button onClick ={this.switchNameHandler}> Switch Name </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My Hobbies are Coding, traveling and reading </Person>
       </div>
     );
   }
